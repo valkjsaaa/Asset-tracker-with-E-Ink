4ad17d16078598426cda1cc4f8d283f73d3fe1db
valkjsaaa <jackieyang51@gmail.com>
2017-04-18 01:25:31 -0700

    use Adafruit GFX to provide extra graphics functionality

--- src/Tag.ino
+++ src/Tag.ino
@@ -4,6 +4,8 @@
 // This #include statement was automatically added by the Particle IDE.
 #include "tcon_mpico.h"
 
+#include "Adafruit_GFX.h"
+
 #define SS_PIN D5
 #define SPI_SPEED 400000
 #define _TCBUSY_PIN D6
@@ -11,6 +13,10 @@
 TCON_MPICO mpico(SS_PIN, SPI_SPEED, _TCBUSY_PIN);
 
 long last_entry=0;
+
+GFXcanvas1 canvas(400, 300);
+
+static char text[] = "Hello World!";
 
 void setup() {
   pinMode(D7, OUTPUT);
@@ -20,13 +26,19 @@ void setup() {
     mpico.sendCommand(resetDataPointer,3);
     Serial.print("resetDataPointer: ");
     mpico.getResponse(-1);
-
-	mpico.uploadImage(squirrel_xbm);
+  canvas.fillScreen(0);
+  canvas.setTextSize(3);
+  canvas.setTextColor(1);
+  canvas.setCursor(0,0);
+  for (int i = 0; i < sizeof(text) / sizeof(text[0]); i++) {
+    canvas.write(text[i]);
+  }
+	mpico.uploadImage((char*)canvas.getBuffer());
 	Serial.println("Upload Image...");
 
-    mpico.sendCommand(displayUpdate,3);
-    Serial.print("Display update... ");
-    mpico.getResponse(-1);
+  mpico.sendCommand(displayUpdate,3);
+  Serial.print("Display update... ");
+  mpico.getResponse(-1);
 }
 
 void loop() {
